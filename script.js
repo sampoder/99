ab5735f54b6c71f161419457e5021c2a440e65bf
Sam Poder <sam.r.poder@gmail.com>
2020-06-14 00:08:49 +0000

    it half works

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 function setup() {
+  
   const values = Object.values(mainData)
   const randomValue = values[parseInt(Math.random() * values.length)]
   var player;
-  function onYouTubePlayerAPIReady() {
-      player = new YT.Player('player', {
-        width: '640',
-        height: '390',
-        videoId: '0Bmhjf0rKe8',
-        events: {
-          onReady: onPlayerReady,
-          onStateChange: onPlayerStateChange
-        }
-      });
-  }
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://friendly-words.glitch.me/word-pairs", true);
+  request.onload = function() {
+    
+    var div_id_random = JSON.parse(this.response)[0];
+    console.log(div_id_random)
+    document.getElementById('player').innerHTML=String("<div id = '").concat(String(div_id_random),"'></div><button id='de' onclick='setup()'>Watch</button>")
+    
+    player = new YT.Player(String(div_id_random), {
+      width: '95%',
+      videoId: randomValue['ID'],
+      playerVars: {
+        end: randomValue['Time Seconds']- 10
 
-  // autoplay video
-  function onPlayerReady(event) {
-      event.target.playVideo();
-  }
-
-  // when video ends
-  function onPlayerStateChange(event) {        
-      if(event.data === 0) {          
-          alert('done');
+      },
+      events: {
+        onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange
       }
-  }
+    });
+    
+
+    // autoplay video
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
+
+    // when video ends
+    function onPlayerStateChange(event) {        
+        if(event.data === 0) {          
+            alert('done');
+        }
+    }
+
+  } 
+  request.send(); 
 
 }
